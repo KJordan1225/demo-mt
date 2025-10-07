2a1ef39fad74184f0f3cd11ffdcd8f059a695676
Keith Jordan <shadow902@gmail.com>
2025-10-07 03:01:01 -0400

    add demotenantseeder to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\DemoTenantsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Run other seeders in order
+        $this->call([
+            DemoTenantsSeeder::class,
+            // RolesSeeder::class,
+            // UsersSeeder::class,
+            // CategoriesSeeder::class,
+            // PostsSeeder::class,
         ]);
     }
 }
